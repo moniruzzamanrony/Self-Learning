d41c34df3aca6eb612e53b7371a8539e1dcab78c
Moniruzzaman Rony <eproni29@gmail.com>
2021-07-09 07:04:44 +0600

    -- Used Filter()

--- Searching-Algo/src/com/self/BinarySearch.java
+++ Searching-Algo/src/com/self/BinarySearch.java
@@ -1,13 +1,15 @@
 package com.self;
 
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class BinarySearch {
     public static void main(String[] args) {
         System.out.println(binarySearch(List.of(1,2,34,5,6), 123));
     }
     private static int binarySearch(List<Integer> dataSet, int target) {
-
         return dataSet.stream().filter(res-> res == target).findFirst().orElse(-1);
 
     }
---
+++ java8/src/com/self/functional/programming/MapAndCollectionClass2.java
@@ -0,0 +1,8 @@
+package com.self.functional.programming;
+
+public class MapAndCollectionClass2 {
+    public static void main(String[] args) {
+
+    }
+}
+
